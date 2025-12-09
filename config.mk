b912c7385b0979225ff8c13bdd219980fceef787
fractal27 <giacomo.dominici07@gmail.com>
2025-12-10 00:26:49 +0100

    test of putting the file to make a standalone

--- config.mk
+++ config.mk
@@ -2,7 +2,7 @@
 
 # PATH_TO_HERBE=$(PWD)/herbe
 SOURCES = client.c server.c gpg-util.c
-LIBS = `pkgconf --libs gpgme`#`pkgconf --libs openssl`
+LIBS = -L ./lib -lgpgme
 CFLAGS=-Wall -O2 -DDEBUG -ggdb
 
 
